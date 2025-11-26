4814a4b2811c8e793c56e408573798223adc9ef8
Rick Hesp <s1179208@student.windesheim.nl>
2025-11-26 16:42:01 +0100

    changed playfield, fixed width calculation
    made a 240x240 field with room for boats on the side the playfield is now 6x6.
    
    fixed the width calculation to actually calculate the right value
    
    changed name to grid_init so we have it the same everywhere
    
    added comments for clarity

--- src/drawGrid.h
+++ src/drawGrid.h
@@ -10,23 +10,24 @@ Adafruit_ILI9341 tft(TFT_CS, TFT_DC, TFT_RST);
 
 
 
-void MakeGrid(){
+void grid_init(){
     tft.begin();
-    tft.setRotation(1);
+    tft.setRotation(1); //320x240
 
-    int w = tft.width();
-    int h = tft.height();
+    //make the play field a square (240x240)
+    int w = 240; //240px
+    int h = tft.height(); //240px
 
-    int horizontal_val = h/7;
-    int vertical_val = h/7;
+    int horizontal_val = w/6;
+    int vertical_val = h/6;
 
-    //start drawing
+    //start drawing (6x6 field)
     tft.fillScreen(ILI9341_BLUE);
-    for (int i = 0; i <= w; i+=vertical_val) {
+    for (int i = 0; i <= w; i+=vertical_val) {//make vertical lines all h/6 apart
         tft.drawFastVLine(i, 0, h, ILI9341_BLACK);
     }
     
-    for(int i = 0; i <= h; i+=horizontal_val){
+    for(int i = 0; i <= h; i+=horizontal_val){ //make horizontal lines all w/6 apart
         tft.drawFastHLine(0, i, w, ILI9341_BLACK);
     }
 }
