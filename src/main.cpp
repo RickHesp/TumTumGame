26b0660e99e60b1482cbeadccfa18b1e4104ee38
Rick Hesp <s1179208@student.windesheim.nl>
2025-11-26 15:53:24 +0100

    moved code to different file

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,10 @@
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ILI9341.h>
+#include "drawGrid.h"
 
-#define TFT_CS   10
-#define TFT_DC   9
-#define TFT_RST  8
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 
-void setup() {
-    SPI.begin();
-    SPI.beginTransaction(SPISettings(240000, MSBFIRST, SPI_MODE0));
-    tft.begin();
-    tft.setRotation(1); //landscape
+int main(void){
+    init();
+    MakeGrid();
 
-    int w = tft.width(); 
-    int h = tft.height();
-
-    int horizontal_val = h/7;
-    int vertical_val = h/7;
-
-    //start drawing
-    tft.fillScreen(ILI9341_BLUE);
-    for (int i = 0; i <= w; i+=vertical_val) {
-        Serial.println("else");
-        tft.drawFastVLine(i, 0, h, ILI9341_BLACK);
-    }
-    
-    for(int i = 0; i <= h; i+=horizontal_val){
-        tft.drawFastHLine(0, i, w, ILI9341_BLACK);
-
-    }
-}
-
-void loop() {
-    
+    return 0;
 }
