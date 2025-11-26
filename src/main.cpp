7269086e085b66418845ab82abd2a024c0d13ece
Rick Hesp <s1179208@student.windesheim.nl>
2025-11-26 15:32:05 +0100

    added grid layout
    known bug:
    flickering of the screen
    weird small grids to the left of the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TFT_CS   10
+#define TFT_DC   9
+#define TFT_RST  8
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    SPI.begin();
+    SPI.beginTransaction(SPISettings(240000, MSBFIRST, SPI_MODE0));
+    tft.begin();
+    tft.setRotation(1); //landscape
+
+    int w = tft.width(); 
+    int h = tft.height();
+
+    int horizontal_val = h/7;
+    int vertical_val = h/7;
+    for (int i = 0; i <= w; i+=vertical_val) {
+        Serial.println("else");
+        tft.drawFastVLine(i, 0, h, ILI9341_BLACK);
+    }
+    
+    for(int i = 0; i <= h; i+=horizontal_val){
+        tft.drawFastHLine(0, i, w, ILI9341_BLACK);
+
+    }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    
 }
