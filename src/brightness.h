3537bb19e8242e075dbeb4398083a30801f0d4bb
ellopatat <117219944+ellopatat@users.noreply.github.com>
2025-12-02 12:19:24 +0100

    Switch PWM to Timer2 and 8-bit ADC
    Updated PWM setup to use Timer2 registers instead of Timer0 for brightness control. ADC interrupt now uses 8-bits. changed the brightness calculations to reflect this.

--- src/brightness.h
+++ src/brightness.h
@@ -4,15 +4,15 @@
 static void pwm_brightness_setup(void){
     //PD5 as output
     DDRD |= (1<<PD5);
-
-    TCCR0A |= (1 << WGM00) | (1 << WGM01);
-    TCCR0B &= ~(1 << WGM02);
-
-    TCCR0A |= (1 << COM0B1);
-    TCCR0A &= ~(1 << COM0B0);
-
-    TCCR0B |= (0 << CS01) | (1 << CS00); 
-    OCR0B = 255;
+    //fast PWM mode.
+    TCCR2A |= (1 << WGM20) | (1 << WGM21);
+    TCCR2B &= ~(1 << WGM22);
+    //non-inverting mode on OC2B(PD5)
+    TCCR2A |= (1 << COM2B1);
+    TCCR2A &= ~(1 << COM2B0);
+    //no prescaler
+    TCCR2B |= (0 << CS21) | (1 << CS20); 
+    OCR2B = 255;
 }
 
 void backlight_set(uint8_t brightness)
@@ -23,15 +23,15 @@ void backlight_set(uint8_t brightness)
 static void adc_setup(void){
     ADMUX  = (0<<MUX0);//ADC channel 0(PC0).
     ADMUX = (1<<REFS0);//AVcc as reference.
+    ADMUX |= (1<<ADLAR);//use only 8 bits for ADC
 
     ADCSRA = (1<<ADEN) | (1<<ADIE) | (1<<ADATE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);//enable: adc, interrupt, auto-trigger, prescaler 128 for 125kHz
     ADCSRA |= (1<<ADSC);//start adc measurements
     sei();
 }
 
-ISR(ADC_vect){
-    uint16_t pot = ADC;//value up to 1023
-    uint8_t brightness = (uint8_t)(pot / 4);//convert to 255 max
+ISR(ADC_vect){  
+    uint8_t brightness = ADC;
     backlight_set(brightness);
 }
 
